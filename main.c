198b7414d7099c83421d0618c100e556b34b66ad
Sohaila26 <sohilaa421@gmail.com>
2024-09-18 21:22:12 +0300

    v1.0.0

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Hamada 10
+
+int main ()
+{
+	int y = 45;
+	int x = Hamada;
+	printf("X value = %i\n",x);
+	printf("Y value = %i\n",y);
+	return 0;
+}
